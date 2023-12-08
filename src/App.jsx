2a423435e29e9f3a0ae8c67585511a7f5374d094
Błażej Mzyk <blazej.mzyk@gmail.com>
2023-12-08 15:09:38 +0100

    Added line through while checking item

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,18 @@ function App() {
     setItems((items) => [...items, item]);
   }
 
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <>
       <VStack justifyContent="center">
@@ -20,7 +32,11 @@ function App() {
           I need to take:
         </Text>
         <Form onAddItems={handleAddItems} />
-        <PackingList items={items} />
+        <PackingList
+          items={items}
+          onDeleteItem={handleDeleteItems}
+          onToggleItem={handleToggleItem}
+        />
         <Stats />
       </VStack>
     </>
--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,15 +1,32 @@
-import { Checkbox, CheckboxGroup, Stack } from "@chakra-ui/react";
+import {
+  Checkbox,
+  Button,
+  CheckboxGroup,
+  Stack,
+  HStack,
+} from "@chakra-ui/react";
 
-const PackingList = ({ items }) => {
+import { SmallCloseIcon } from "@chakra-ui/icons";
+
+const PackingList = ({ items, onDeleteItem, onToggleItem }) => {
   console.log(items);
   return (
     <>
       <CheckboxGroup colorScheme="green">
         <Stack spacing={[1, 5]} direction={["column", "row"]}>
           {items.map((item) => (
-            <Checkbox key={item.id}>
-              {item.quantity} {item.description}
-            </Checkbox>
+            <HStack key={item.id}>
+              <Checkbox
+                value={item.packed}
+                onChange={() => onToggleItem(item.id)}
+                textDecoration={item.packed ? "line-through" : null}
+              >
+                {item.quantity} {item.description}
+              </Checkbox>
+              <button onClick={() => onDeleteItem(item.id)}>
+                <SmallCloseIcon />
+              </button>
+            </HStack>
           ))}
         </Stack>
       </CheckboxGroup>
